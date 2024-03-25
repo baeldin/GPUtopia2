8042c8601bc17425e1e0ba2ca6061371b5e3c14a
baeldin <ph.scheffknecht@gmail.com>
2024-03-25 19:55:25 +0100

    add alpha to color struct operators

--- GPUtopia2/color.h
+++ GPUtopia2/color.h
@@ -13,7 +13,7 @@ struct color
     float r;
     float g;
     float b;
-    float a = 1.;
+    float a = 1.f;
     color() : r(0.f), g(0.f), b(0.f), a(1.f) {}
     color(const float x) : r(x), g(x), b(x), a(1.f) {}
     color(const float c, const float a_) : r(c), g(c), b(c), a(a_) {}
@@ -26,13 +26,12 @@ struct color
         r = r_; g = g_; b = b_; a = a_;
     }
     color operator+ (const color& c) {
-        return color(r + c.r, g + c.g, b + c.b);
-    }
+        return color(r + c.r, g + c.g, b + c.b, a + c.a); }
     color operator* (const float& x) { 
-        return color(r * x, g * x, b * x); }
+        return color(r * x, g * x, b * x, a * x); }
     color operator/ (const float& x) { 
         const float inv_x = 1.f / x;
-        return color(r * inv_x, g * inv_x, b * inv_x); }
+        return color(r * inv_x, g * inv_x, b * inv_x, a * inv_x); }
     color& operator= (const color& c)
     {
         if (this != &c) {
@@ -46,7 +45,7 @@ struct color
 
 inline const bool operator==(const color& lhs, const color& rhs)
 {
-    return(lhs.r == rhs.r && lhs.g == rhs.g && lhs.b == rhs.b);
+    return(lhs.r == rhs.r && lhs.g == rhs.g && lhs.b == rhs.b, lhs.a == rhs.a);
 }
 
 color clamp(const color& c);
