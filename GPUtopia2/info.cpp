1557e51f7c423d8bcc85c0041797ce444918d25a
baeldin <ph.scheffknecht@gmail.com>
2025-01-09 09:02:58 +0100

    Add status display to info window

--- GPUtopia2/info.cpp
+++ GPUtopia2/info.cpp
@@ -17,6 +17,29 @@ void infoWindow(clFractal& cf)
 		ImGui::InputTextMultiline("##source", text, IM_ARRAYSIZE(text), ImVec2(-FLT_MIN, ImGui::GetTextLineHeight() * 60), flags);
 		ImGui::TreePop();
 	}
+	bool ret = false;
+	ImGui::BulletText(
+		"Fractal Status:\n"
+		"- Render Kernel Running = %d\n"
+		"- Image Kernel Running = %d\n"
+		"- Render Kernel Run Requested = %d\n"
+		"- Image Kernel Run Requested = %d\n"
+		"- Running = %d\n"
+		"- Done = %d\n",
+		ret,
+		cf.status.kernelRunning,
+		cf.status.imgKernelRunning,
+		cf.status.runKernel,
+		cf.status.runImgKernel,
+		cf.running(),
+		cf.status.done
+	);
+	// PROGRESS BAR
+	static float progress = 0.f;
+	progress = (float)cf.image.current_sample_count / (float)cf.image.target_sample_count;
+	ImGui::Text("Render Progress:");
+	ImGui::ProgressBar(progress, ImVec2(0.0f, 0.0f));
+
 
 	ImGui::End();
 }
