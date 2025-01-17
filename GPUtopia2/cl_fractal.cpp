76de3d378ead936310b548a5e0ea8d0ffc8f3718
baeldin <ph.scheffknecht@gmail.com>
2025-01-17 01:22:32 +0100

    fix wrong calculation of img to complex space

--- GPUtopia2/cl_fractal.cpp
+++ GPUtopia2/cl_fractal.cpp
@@ -10,7 +10,7 @@ void clFractalImage::updateComplexSubplane()
 Complex<float> clFractalImage::image2complex(const cl_float2 xy) const
 {
     const Complex<float> relativeImgCoord = Complex<float>(
-        xy.x / (float)this->size.x - 0.5, 
-        xy.y / (float)this->size.y - 0.5);
+        (xy.x / (float)this->size.x - 0.5) * this->span.x, 
+        ((this->size.y - xy.y) / (float)this->size.y - 0.5) * this->span.y);
     return relativeImgCoord * this->rotation + this->center;
 }
