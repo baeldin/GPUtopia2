7b5b051063a431d76f2eefcc8c7df514205eed3c
baeldin <ph.scheffknecht@gmail.com>
2025-01-20 08:37:28 +0100

    remove unneded checkboxes in info panel

--- GPUtopia2/info.cpp
+++ GPUtopia2/info.cpp
@@ -8,13 +8,14 @@ void infoWindow(clFractal& cf, fractalNavigationParameters& nav)
 	{
 		//ImGui::TextWrapped("lolwhat"); // cf.fullCLcode.c_str());
 		static char* text = (char*)" ";
+		static uint32_t txtLen = 0;
 		if (ImGui::Button("reload")) {
 			text = cf.fullCLcode.data();
 		}
-		static ImGuiInputTextFlags flags = ImGuiInputTextFlags_AllowTabInput;
-		ImGui::CheckboxFlags("ImGuiInputTextFlags_ReadOnly", &flags, ImGuiInputTextFlags_ReadOnly);
-		ImGui::CheckboxFlags("ImGuiInputTextFlags_AllowTabInput", &flags, ImGuiInputTextFlags_AllowTabInput);
-		ImGui::CheckboxFlags("ImGuiInputTextFlags_CtrlEnterForNewLine", &flags, ImGuiInputTextFlags_CtrlEnterForNewLine);
+		static ImGuiInputTextFlags flags = 1 << 14; // read only ImGuiInputTextFlags_AllowTabInput;
+		//ImGui::CheckboxFlags("ImGuiInputTextFlags_ReadOnly", &flags, ImGuiInputTextFlags_ReadOnly);
+		//ImGui::CheckboxFlags("ImGuiInputTextFlags_AllowTabInput", &flags, ImGuiInputTextFlags_AllowTabInput);
+		//ImGui::CheckboxFlags("ImGuiInputTextFlags_CtrlEnterForNewLine", &flags, ImGuiInputTextFlags_CtrlEnterForNewLine);
 		ImGui::InputTextMultiline("##source", text, IM_ARRAYSIZE(text), ImVec2(-FLT_MIN, ImGui::GetTextLineHeight() * 60), flags);
 		ImGui::TreePop();
 	}
