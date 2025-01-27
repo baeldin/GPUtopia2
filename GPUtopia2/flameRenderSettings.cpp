d0f732d775c0813ad421e24d6fec70a2e2e4aef6
baeldin <ph.scheffknecht@gmail.com>
2025-01-27 06:07:17 +0100

    make point selection a combo

--- GPUtopia2/flameRenderSettings.cpp
+++ GPUtopia2/flameRenderSettings.cpp
@@ -6,7 +6,8 @@ void flameRenderSettingsWindow(clFractal& cf)
 	ImGui::InputFloat("Brightness", &cf.flameRenderSettings.x, 0.1f, 10.f);
 	ImGui::InputFloat("Gamma", &cf.flameRenderSettings.y, 0.1f, 10.f);
 	ImGui::InputFloat("Vibrancy", &cf.flameRenderSettings.z, 0.1f, 10.f);
-	ImGui::InputInt("Point Selection", &cf.flamePointSelection, 0, 2);
+	static char* pointSelection[] = { (char*)"All", (char*)"Outside", (char*)"Inside" };
+	ImGui::Combo("Point Selection", &cf.flamePointSelection, pointSelection, IM_ARRAYSIZE(pointSelection));
 	ImGui::InputInt("Warmup", &cf.flameWarmup, 0, 2);
 	ImGui::End();
 }
