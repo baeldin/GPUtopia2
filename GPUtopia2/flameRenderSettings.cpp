2706ac2f461c0cff6a553a7c8e6e1401d707f78b
baeldin <ph.scheffknecht@gmail.com>
2025-02-06 01:33:01 +0100

    add Ui element to chose samples per kernel run

--- GPUtopia2/flameRenderSettings.cpp
+++ GPUtopia2/flameRenderSettings.cpp
@@ -2,12 +2,13 @@
 
 void flameRenderSettingsWindow(clFractal& cf)
 {
-	ImGui::Begin("Flame Render Settings");
+	ImGui::Begin("Render Settings");
 	ImGui::InputFloat("Brightness", &cf.flameRenderSettings.x, 0.1f, 10.f);
 	ImGui::InputFloat("Gamma", &cf.flameRenderSettings.y, 0.1f, 10.f);
 	ImGui::InputFloat("Vibrancy", &cf.flameRenderSettings.z, 0.1f, 10.f);
 	static char* pointSelection[] = { (char*)"All", (char*)"Outside", (char*)"Inside" };
 	ImGui::Combo("Point Selection", &cf.flamePointSelection, pointSelection, IM_ARRAYSIZE(pointSelection));
 	ImGui::InputInt("Warmup", &cf.flameWarmup, 0, 2);
+	ImGui::DragInt("Samples per kernel call", &cf.samples_per_kernel_run, 1, 1, cf.image.target_sample_count);
 	ImGui::End();
 }
