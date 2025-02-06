28e166a1c99f9229096b4555a25dd264b95ee73a
baeldin <ph.scheffknecht@gmail.com>
2025-02-06 13:12:02 +0100

    move open/save/export to separate file

---
+++ GPUtopia2/fileOperations.cpp
@@ -0,0 +1,79 @@
+#include "fileOperations.h"
+
+void newCLF(clFractal& cf, clCore& cc)
+{
+	cf = clFractal();
+	cf.makeCLCode(NEW_FILES);
+	cf.image.size.x = 1280;
+	cf.image.size.y = 720;
+	cf.image.resetStatus();
+	cf.image.updateComplexSubplane();
+	cc.resetCore();
+	cc.compileFractalKernel(cf.fullCLcode);
+}
+
+void openCLF(clFractal& cf, clCore& cc)
+{
+	std::string path;
+	bool success = false;
+
+	openFileDialog(path, success, L"clf", L"GPUtopia Fractals (*.clf)");
+	if (success)
+	{
+		std::ifstream inFile(path);
+		std:cout << path << "\n";
+		std::string jsonStr;
+		inFile >> jsonStr;
+		json back_json = json::parse(jsonStr);
+		auto cfm = back_json.get<clFractalMinimal>();
+		cf = clFractal(cfm);
+		cf.makeCLCode(SAME_FILES);
+		cc.resetCore();
+		cc.compileFractalKernel(cf.fullCLcode);
+	}
+}
+
+void saveCLF(clFractal& cf)
+{
+	bool success = false;
+	std::string path;
+	saveFileDialog(path, success, L"clf", L"GPUtopia Fractals (*.clf)");
+	if (success)
+	{
+		json json = cf.toExport();
+		std::ofstream outFile(path);
+		outFile << json.dump();
+		outFile.close();
+	}
+}
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <stb_image_write.h>
+
+void savePNG(const std::vector<color>& image_data, const cl_int2& size)
+{
+	bool success = false;
+	std::string fileName;
+	saveFileDialog(fileName, success, L"png", L"Portable Network Graphics (*.png)");
+	if (success)
+	{
+		std::vector<uint8_t> image_sRGB(3 * size.x * size.y);
+		float fMax = 0.f;
+		uint32_t iMax = 0;
+		for (unsigned int i = 0; i < 3 * size.x * size.y; i += 3) {
+			color col = image_data[i / 3];
+			image_sRGB[i] = uint8_t(col.r * 255);
+			image_sRGB[i + 1] = uint8_t(col.g * 255);
+			image_sRGB[i + 2] = uint8_t(col.b * 255);
+			if (col.r > fMax) fMax = col.r;
+			if (col.g > fMax) fMax = col.g;
+			if (col.b > fMax) fMax = col.b;
+			if (image_sRGB[i] > iMax) iMax = image_sRGB[i];
+			if (image_sRGB[i + 1] > iMax) iMax = image_sRGB[i + 1];
+			if (image_sRGB[i + 2] > iMax) iMax = image_sRGB[i + 2];
+		}
+		stbi_write_png((char*)fileName.c_str(), size.x, size.y, 3, &image_sRGB[0], size.x * 3);
+	}
+}
---
+++ GPUtopia2/fileOperations.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#define _CRT_SECURE_NO_WARNINGS 
+
+#include "cl_fractal.h"
+#include "cl_core.h"
+
+
+// forward declaration needed to avoid syntax error!?
+class clCore; 
+
+void newCLF(clFractal& cf, clCore& cc);
+void openCLF(clFractal& cf, clCore& cc);
+void saveCLF(clFractal& cf);
+void savePNG(const std::vector<color>& image_data, const cl_int2& size);
