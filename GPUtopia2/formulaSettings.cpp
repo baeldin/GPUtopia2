47444ff062d401517b7e4d56adc86c4c8749f26b
baeldin <ph.scheffknecht@gmail.com>
2025-02-10 16:49:06 +0100

    remove gradient stuff from the formula window

--- GPUtopia2/formulaSettings.cpp
+++ GPUtopia2/formulaSettings.cpp
@@ -74,41 +74,7 @@ void formulaSettingsWindow(clFractal& cf, clCore& cc)
 	{
 		ImGui::DragFloat(key.c_str(), &val.first, 0.01f, -100.f, 100.f);
 	}
-	static std::vector<color> gradientImg;
-	static GLuint gradientTextureID;
-	static bool needGradientTexture;
-	static Gradient gradUpdate;
-	static int colorNodeCount = 4;
-	static int currentGradientIndex = 2;
-	static int previousGradientIndex = -1; // make sure that this is different initially
-	const char* items[] = { "UF Default", "UF Default Muted", "Volcano Under a Glacier", "Volcano Under a Glacier2", "Jet", "CBR_coldhot", "nice_random", "Default" , "Test"};
-	static std::vector<color> gradient_img_data(400 * 30, color(0.f));
-	static std::vector<Gradient> gradients = { uf_default, standard_muted, volcano_under_a_glacier, volcano_under_a_glacier2, jet, CBR_coldhot, nice_random, Gradient(), test };
-	if (previousGradientIndex != currentGradientIndex)
-	{
-		gradUpdate = gradients[currentGradientIndex];
-		previousGradientIndex = currentGradientIndex;
-	}
-	ImGui::Combo("Gradent", &currentGradientIndex, items, IM_ARRAYSIZE(items));
-	ImGui::DragInt("Gradient Nodes", &colorNodeCount, 1, 12);
-	if (ImGui::Button("Test Gradient"))
-	{
-		//cf.gradient = Gradient();
-		gradUpdate = randomGradient(colorNodeCount);
-		gradUpdate.print();
-		// grad.printNodes();
-		// grad.printFine();
-	}
-	if (cf.gradient != gradUpdate)
-	{
-		cf.gradient = gradUpdate;
-		cf.status.runImgKernel = true;
-		glDeleteTextures(1, &gradientTextureID);
-		gradientImg = cf.gradient.getGradientImg(400, 20); // cf.gradient.getGradientImg(400, 20);
-		makeTexture(gradientTextureID, 400, 20, gradientImg);
-		needGradientTexture = false;
-	}
-	ImGui::Image((void*)(intptr_t)gradientTextureID, ImVec2(400, 20));
+
 	ImGui::Checkbox("Vomit pixel contents", &cf.vomit);
 	ImGui::End();
 }
